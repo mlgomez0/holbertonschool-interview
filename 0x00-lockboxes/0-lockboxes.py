038f7554f4b4234a9ed0ad36376d1b3fb4eabe98
mlgomez0 <mlgomez230@hotmail.com>
2020-07-18 20:14:56 +0000

    simplify code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,33 +4,23 @@
 
 def canUnlockAll(boxes):
     """returns True or False"""
-    list_keys = []
-    box_stack = boxes[0]
-    temp_stack = []
-    i = 0
-    n = 0
+    if len(boxes[0]) == 0:
+        return False
+    list_keys = [x for x in boxes[0] if x != 0]
 
-    while len(box_stack) != 0:
-        box_open = box_stack.pop()
-        if type(box_open) != list:
-            if box_open not in list_keys:
-                list_keys.append(box_open)
-                temp_stack.append(boxes[box_open])
-        else:
-            for key in box_open:
-                if key not in list_keys and key != 0:
-                    list_keys.append(key)
-                    temp_stack.append(boxes[key])
-        if len(box_stack) == 0 and len(temp_stack) != 0:
-            box_stack = [row for row in temp_stack]
-            temp_stack = []
-    list_keys.sort()
-    if list_keys[0] == 1:
-        i = 1
-
-    while i < len(boxes):
-        if i != list_keys[n]:
-            return False
-        i += 1
-        n += 1
-    return True
+    for i in list_keys:
+        if boxes[i]:
+            box = boxes[i]
+            for j in box:
+                if j not in list_keys and j != 0:
+                    list_keys.append(j)
+    if len(list_keys) != 0:
+        list_keys.sort()
+        key = 1
+        while key <= len(list_keys):
+            if key != list_keys[key-1]:
+                return False
+            key += 1
+        return True
+    else:
+        return False
