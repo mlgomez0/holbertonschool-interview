fce9554155a583c0603ab688373aef8e2adde9b9
mlgomez0 <mlgomez230@hotmail.com>
2020-08-15 19:37:57 +0000

    fix topplings

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -2,13 +2,15 @@
 
 /**
  * sandpiles_sum - computes the sum of two sandpiles
- * @grid1[3][3]: grid to sum up
- * @grid2[3][3]: grid to sum up
+ * @grid1: grid to sum up
+ * @grid2: grid to sum up
  */
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
+	int grid_copy[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -20,11 +22,12 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	{
 		printf("=\n");
 		print_grid(grid1);
+		copy_grid(grid_copy, grid1);
 		for (i = 0; i < 3; i++)
 		{
 			for (j = 0; j < 3; j++)
 			{
-				if (grid1[i][j] > 3)
+				if (grid_copy[i][j] > 3)
 				{
 					grid1[i][j] = grid1[i][j] - 4;
 					if (i != 0)
@@ -43,12 +46,13 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 
 /**
  * check_stable - computes the sum of two sandpiles
- * @grid1[3][3]: grid to sum up
- * @grid2[3][3]: grid to sum up
+ * @grid1: grid to sum up
+ * Return: one or cero
  */
 int check_stable(int grid1[3][3])
 {
 	int i, j;
+
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -63,12 +67,13 @@ int check_stable(int grid1[3][3])
 
 /**
  * print_grid - print a sandpiles
- * @grid1[3][3]: grid to sum up
+ * @grid: grid to sum up
  */
 
 static void print_grid(int grid[3][3])
 {
 	int i, j;
+
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -78,5 +83,24 @@ static void print_grid(int grid[3][3])
 			printf("%d", grid[i][j]);
 		}
 		printf("\n");
+	}
+}
+
+/**
+ * copy_grid- makes a copy of the grid
+ * @grid_copy: where copy will be
+ * @grid1: grid to copy
+ */
+
+void copy_grid(int grid_copy[3][3], int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid_copy[i][j] = grid1[i][j];
+		}
 	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -7,5 +7,6 @@
 int check_stable(int grid1[3][3]);
 static void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void copy_grid(int grid_copy[3][3], int grid1[3][3]);
 
 #endif
