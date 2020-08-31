f3ed9be83516e00ee2a483394b352ab04cb62f21
mlgomez0 <mlgomez230@hotmail.com>
2020-08-31 15:17:35 +0000

    using try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python3
-"""hshs"""
+""" script that reads stdin line
+    by line, computes metrics"""
 import sys
 
-status = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0,
-}
+
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 file_size = 0
-con = 1
+num = 1
 try:
     for line in sys.stdin:
-        token = line.split()
-        if len(token) > 2:
-            file_s = token[-1]
-            status_code = token[-2]
-            file_size += int(file_s)
-            if status_code in status:
-                status[status_code] += 1
-        if con % 10 == 0:
+        args = line.split()
+        if len(args) > 2:
+            file_size = file_size + int(args[-1])
+            if args[-2] in status_codes:
+                st = args[-2]
+                status_codes[st] += 1
+        if num % 10 == 0:
             print('File size: {}'.format(file_size))
-            for k, v in sorted(status.items()):
+            for k, v in sorted(status_codes.items()):
                 if v != 0:
                     print('{}: {}'.format(k, v))
-        con += 1
+        num += 1
 except KeyboardInterrupt:
     pass
 finally:
     print('File size: {}'.format(file_size))
-    for k, v in sorted(status.items()):
+    for k, v in sorted(status_codes.items()):
         if v != 0:
             print('{}: {}'.format(k, v))
