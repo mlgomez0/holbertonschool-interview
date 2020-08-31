006963a842abbb96d2e424337387691823a04917
mlgomez0 <mlgomez230@hotmail.com>
2020-08-31 14:58:16 +0000

    using try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,15 +4,17 @@
 import sys
 
 
-status_codes = {"200":0, "301":0, "400":0, "401":0, "403":0, "404":0, "405":0, "500":0}
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 file_size = 0
 num = 1
 try:
     for line in sys.stdin:
         args = line.split(' ')
-        file_size = file_size + int(args[-1])
-        if args[-2] in status_codes:
-            status_codes[args[-2]] += 1
+        if len(args) >= 2:
+            file_size = file_size + int(args[-1])
+            if args[-2] in status_codes:
+                status_codes[args[-2]] += 1
         if num % 10 == 0:
             print('File size: {}'.format(file_size))
             for k, v in sorted(status_codes.items()):
