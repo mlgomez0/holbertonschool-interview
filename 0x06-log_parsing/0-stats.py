3248b737c324449e1f2a2fbd9adb2053f13b3ce0
mlgomez0 <mlgomez230@hotmail.com>
2020-08-31 14:33:28 +0000

    using try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,27 +5,30 @@ import sys
 import signal
 
 
-def sig_handler(sig, frame):
-    """handles the signal"""
+def exit_gracefully(file_size, status_codes):
+
     print('File size: {}'.format(file_size))
     for k, v in sorted(status_codes.items()):
         print('{}: {}'.format(k, v))
     sys.exit(0)
 
-
 status_codes = {}
 file_size = 0
 num = 1
-for line in sys.stdin:
-    args = line.split(' ')
-    file_size = file_size + int(args[-1])
-    if args[-2] in status_codes:
-        status_codes[args[-2]] += 1
-    else:
-        status_codes[args[-2]] = 1
-    if num % 10 == 0:
-        print('File size: {}'.format(file_size))
-        for k, v in sorted(status_codes.items()):
-            print('{}: {}'.format(k, v))
-    num += 1
-    signal.signal(signal.SIGINT, sig_handler)
+try:
+    for line in sys.stdin:
+        args = line.split(' ')
+        file_size = file_size + int(args[-1])
+        if args[-2] in status_codes:
+            status_codes[args[-2]] += 1
+        else:
+            status_codes[args[-2]] = 1
+        if num % 10 == 0:
+            print('File size: {}'.format(file_size))
+            for k, v in sorted(status_codes.items()):
+                print('{}: {}'.format(k, v))
+        num += 1
+except KeyboardInterrupt:
+    pass
+finally:
+    exit_gracefully(file_size, status_codes)
