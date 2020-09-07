245a3c1d5a827da5e9508158b89ff42d5072bef2
mlgomez0 <mlgomez230@hotmail.com>
2020-09-07 13:56:49 +0000

    change while condition

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -12,7 +12,7 @@ int check_cycle(listint_t *list)
 	listint_t *temp1 = list;
 	listint_t *temp2 = list;
 
-	while (temp1 && temp1->next && temp2->next->next && temp2)
+	while (temp1 && temp2->next && temp2)
 	{
 		temp1 = temp1->next;
 		temp2 = temp2->next->next;
