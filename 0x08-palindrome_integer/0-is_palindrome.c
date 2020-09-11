a45e92f2175b9123af9d213137ca7e7b70b53f03
mlgomez0 <mlgomez230@hotmail.com>
2020-09-11 15:18:04 +0000

    create files

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,9 +1,9 @@
 #include "palindrome.h"
 
 /**
- * is_palindrome - prints all elements of a listint_t list
- * @n: pointer to head of list
- * Return: number of nodes
+ * is_palindrome - determines if a unsigned long is a palintrome
+ * @n: unsigned integer
+ * Return: 1 is palindrome or 0 if not
  */
 
 int is_palindrome(unsigned long n)
